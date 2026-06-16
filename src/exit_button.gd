e20df25a7ad89b65f5a404760e19f76996f372e5
Tactical Laptop Bag <tacticallaptopbag@protonmail.com>
2026-06-16 09:07:48 -0400

    Hide exit button on web

--- src/exit_button.gd
+++ src/exit_button.gd
@@ -3,7 +3,7 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if OS.has_feature("mobile"):
+	if OS.has_feature("mobile") or OS.has_feature("web"):
 		queue_free()
 		return
 	pressed.connect(_on_pressed)
