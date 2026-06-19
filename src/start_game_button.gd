8ed4c9aad867bace9d8a050406e98a08a008d0b4
Tactical Laptop Bag <tacticallaptopbag@protonmail.com>
2026-06-19 14:13:18 -0400

    Fix play button being disabled on standalone builds

--- src/start_game_button.gd
+++ src/start_game_button.gd
@@ -3,13 +3,7 @@ extends Button
 
 func _ready() -> void:
 	pressed.connect(_on_pressed)
-	PuzzleProvider.puzzle_generated.connect(_on_puzzle_generated)
-	_on_puzzle_generated(PuzzleProvider.get_available_sizes())
 
 
 func _on_pressed():
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
-
-
-func _on_puzzle_generated(available_sizes: Array[int]):
-	disabled = DataPersistence.grid_size not in available_sizes
